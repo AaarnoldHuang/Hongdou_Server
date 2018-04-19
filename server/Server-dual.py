25b496530ea0409a19c3499d2223534be0a6b85b
ArnoldHuang <arnoldhuang0725@gmail.com>
2018-04-19 19:23:29 +0800

    Yeah

--- server/Server-dual.py
+++ server/Server-dual.py
@@ -13,21 +13,49 @@ class hongdouSocketsServer(socketserver.BaseRequestHandler):
             while True:
                 data = tcpCliSock.recv(BUFSIZE)
                 print(data)
-                if data == b'/newUser':
+                if data == b'/newUser':                                     #当选择创建新用户时
+                    tcpCliSock.send('/sure'.encode('utf-8'))
+                    userData = tcpCliSock.recv(BUFSIZE)
+                    userInfo = userData.decode('utf-8').split(' ')          #分割传入信息
+                    print(userInfo)                                         #打印分割后的信息
+                    if newuser(userInfo):                                   #执行新建用户函数，执行成功给客户端发送/success
+                        tcpCliSock.send('/success'.encode('utf-8'))
+                    else:
+                        tcpCliSock.send('/Failed. Name existed'.encode('utf-8'))        #执行失败返回信息
+
+                elif data == b'/newUserTable':                                  #新建表函数，CLient中用不到，调试用
+                    newusertable()
+                    tcpCliSock.send('/success'.encode('utf-8'))
+
+                elif data == b'/newDataTable':
+                    if newdatatable():
+                        tcpCliSock.send('/success'.encode('utf-8'))
+                    else:
+                        tcpCliSock.send('/Failed'.encode('utf-8'))
+
+                elif data == b'/newMessage':
+                    tcpCliSock.send('/sure'.encode('utf-8'))
+                    userData = tcpCliSock.recv(BUFSIZE)
+                    userInfo = userData.decode('utf-8').split(' ')          #传入格式： 用户名 匿名性 标题
+                    print(userInfo)
+                    tcpCliSock.send('/GotInfo'.encode('utf-8'))
+                    messages = tcpCliSock.recv(BUFSIZE).decode('utf-8')     #确认后传入留言内容
+                    print(messages)
+                    if newMessage(userInfo, messages):
+                        tcpCliSock.send('/success'.encode('utf-8'))
+                    else:
+                        tcpCliSock.send('Failed'.encode('utf-8'))
+
+                elif data == b'/login':
                     tcpCliSock.send('/sure'.encode('utf-8'))
                     userData = tcpCliSock.recv(BUFSIZE)
                     userInfo = userData.decode('utf-8').split(' ')
-                    print(userInfo)
-                    if newuser(userInfo):
-                        tcpCliSock.send('/successful'.encode('utf-8'))
+                    if login(userInfo):
+                        tcpCliSock.send('/success'.encode('utf-8'))
                     else:
-                        tcpCliSock.send('/Failed. Name existed'.encode('utf-8'))
+                        tcpCliSock.send('/Failed'.encode('utf-8'))
 
-                elif data == b'/newTable':
-                    newusertable()
-                    tcpCliSock.send('/successful'.encode('utf-8'))
-
-                elif data == b'/test':
+                elif data == b'/test':                                      #测试连接，链接正常返回b'hi'
                     tcpCliSock.send('hi'.encode('utf-8'))
 
                 elif not data:
@@ -35,22 +63,32 @@ class hongdouSocketsServer(socketserver.BaseRequestHandler):
             tcpCliSock.close()
         tcpSerSock.server_close()
 
+#新建用户表函数
 def newusertable():
     cursor = mariadb_connection.cursor()
     try:
         cursor.execute("CREATE TABLE hongdou_user (id int primary key, name varchar(20), \
         password varchar(20), sex varchar(6), postNum int)")
+        mariadb_connection.commit()
+        cursor.close()
     except mariadb.Error as error:
         print("Error: {}".format(error))
-    mariadb_connection.commit()
-    cursor.close()
     return True
 
+#新建数据表函数
 def newdatatable():
+    cursor = mariadb_connection.cursor()
+    try:
+        cursor.execute("CREATE TABLE hongdou_data (id int AUTO_INCREMENT, name varchar(20), \
+        anonymous int, likes int, title varchar(100), message varchar(300), primary key(id))")
+        mariadb_connection.commit()
+        cursor.close()
+        return True
+    except mariadb.Error as error:
+        print("Error: {}".format(error))
+    return False
 
-    return
-
-
+#新建用户函数
 def newuser(userinfo):
     cursor = mariadb_connection.cursor()
     try:
@@ -65,7 +103,22 @@ def newuser(userinfo):
     except mariadb.Error as error:
         print("Error: {}".format(error))
 
+def login(userinfo):
+    cursor = mariadb_connection.cursor()
+    try:
+        cursor.execute("SELECT name, password FROM hongdou_user WHERE \
+        name = %s", (userinfo[0],))
+        data = cursor.fetchall()
+        passwd = data[0]
+        print(data)
+        if passwd[1] == userinfo[1]:
+            return True
+        else:
+            return False
+    except mariadb.Error as error:
+        print("Error: {}".format(error))
 
+#检查用户是否存在函数
 def userexitcheck(username):
     cursor = mariadb_connection.cursor()
     try:
@@ -77,6 +130,18 @@ def userexitcheck(username):
     except mariadb.Error as error:
         print("Error: {}".format(error))
 
+def newMessage(userinfo, message):
+    cursor = mariadb_connection.cursor()
+    try:
+        cursor.execute("INSERT INTO hongdou_data (name, anonymous, likes, title, message) \
+        VALUES (%s, %s, %s, %s, %s)", [userinfo[0], userinfo[1], 0, userinfo[2], message])
+        mariadb_connection.commit()
+        cursor.close()
+        return True
+    except mariadb.Error as error:
+        print("Error: {}".format(error))
+
+#主函数
 if __name__ == '__main__':
     HOST = ''
     PORT = 20566
